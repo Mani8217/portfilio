52195de386db04e6a5d785f7666a108c60a389b9
Mani.Sh <100167532+Mani8217@users.noreply.github.com>
2023-01-23 00:17:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-function myFunction() {
-    var x = document.querySelector(".header-nav");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
+  $(document).ready(function(){
+    $("#icon").click(function(){
+      $("#header-nav").slideDown("slow")
+    })
+  })
